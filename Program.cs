d9be498a185bcdbd28116b8c0acb68f9c01695ba
radostinaatanasova <radostinaatanasova@kpmg.com>
2023-01-22 11:55:49 +0200

    Add solution files

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+       
+        bool isBalanced = false;
+        string prevParantesis = "";
+        string firstParantesis = "";
+
+        int countLeft = 0;
+        int countRight = 0; 
+
+        for (int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+
+            
+            switch (input)
+            {
+                case "(":
+                    if (prevParantesis=="")
+                    {
+                        firstParantesis = "(";
+                        isBalanced = true;
+                        prevParantesis = "(";
+                        countLeft++;
+                        break;
+                    }
+                    if (firstParantesis == "(" && prevParantesis == ")")
+                    {
+                        isBalanced = true;
+                    }
+                    else
+                    {
+                        isBalanced = false;
+                    }
+                    prevParantesis = "(";
+                    countLeft++;
+                    break;
+                case ")":
+                    if (prevParantesis == "")
+                    {
+                        firstParantesis = ")";
+                        prevParantesis = ")";
+                        isBalanced = false;
+                        countRight++;
+                        break;
+                    }
+                    if (firstParantesis == "(" && prevParantesis == "(")
+                    {
+                        isBalanced = true;
+                    }
+                    else
+                    {
+                        isBalanced = false;
+                    }
+                    prevParantesis = ")";
+                    countRight++;
+                    break;
+            }
+        }
+        if (isBalanced && firstParantesis=="(" && prevParantesis == ")" && countRight == countLeft)
+        {
+            Console.WriteLine("BALANCED");
+        }
+        else
+        {
+            Console.WriteLine("UNBALANCED");
+        }
+    }
+}
